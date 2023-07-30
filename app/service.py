f9ab114a09097111902d2b46fa698f517a364929
Mark Arkhipychev <hackyourrself@gmail.com>
2023-07-30 16:24:26 +0300

    saves

---
+++ app/service.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from pandas import DataFrame
+from pydantic import ValidationError
+from typing import Optional
+
+
+class Excel:
+    @classmethod
+    def read_excel(cls, file_path: str, sel_rows: Optional[list] = None) -> DataFrame:
+        df = pd.read_excel(file_path).fillna(" ")
+        if sel_rows:
+            return df[sel_rows]
+        return df
+
+    @classmethod
+    def write_excel(cls):
+        ...
