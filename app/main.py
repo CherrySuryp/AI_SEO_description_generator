4d1c7d97472ba29cacebfc6d1898198fe6634970
Mark Arkhipychev <hackyourrself@gmail.com>
2023-08-02 16:06:50 +0300

    saves

--- app/main.py
+++ app/main.py
@@ -16,4 +16,3 @@ if __name__ == '__main__':
     response = asyncio.run(chatgpt.send_requests(df.excel_to_ai_prompt(df_excel)))
     df_excel = df.ai_result_to_df(df_excel, response)
     df.write_excel(df_excel)
-
