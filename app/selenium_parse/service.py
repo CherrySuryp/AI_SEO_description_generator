0e80341f5e96437d9f6cbc2ea7e3e619953c0efc
Mark Arkhipychev <hackyourrself@gmail.com>
2023-08-28 11:49:30 +0300

    Fixed bugs

--- app/selenium_parse/service.py
+++ app/selenium_parse/service.py
@@ -3,13 +3,13 @@ import os
 import json
 import pickle
 
+from typing import Dict
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
+from selenium.webdriver.support import expected_conditions as ec
 
 from fake_useragent import UserAgent
 
@@ -29,17 +29,25 @@ class MpsParser:
 
         self._driver = webdriver.Chrome(options=options)
         # self._driver.set_window_size(1000, 600)
-        self._driver.maximize_window()
+        # self._driver.maximize_window()
 
     def check_cookies(self) -> bool:
+        """
+        Проверка наличия сохраненное сессии
+        """
         return os.path.exists(self._cookies_path)
 
     def inject_cookies(self) -> None:
+        """
+        Инъекция cookie для авторизации
+        """
         for cookie in pickle.load(open(self._cookies_path, "rb")):
             self._driver.add_cookie(cookie)
-        print("Cookies injected")
 
     def authorize(self, save_cookies: bool = False) -> None:
+        """
+        Авторизация на сайте с сохранением cookie в файл
+        """
         self._driver.get("https://mpstats.io/login")
         email_input = self._driver.find_element(By.ID, "email")
         passwd_input = self._driver.find_element(By.NAME, "password")
@@ -55,10 +63,15 @@ class MpsParser:
             print("Cookies saved")
 
     def auth_pipeline(self):
+        """
+        Авторизация и инъекция cookie по условиям
+        """
         if self.check_cookies():
-            self._driver.get("https://mpstats.io")
+            self._driver.get("https://mpstats.io/login")
             self.inject_cookies()
             self._driver.refresh()
+            print("Cookies injected")
+
             if self._driver.current_url == "https://mpstats.io/doubleSession":
                 print("Double Session")
                 self.authorize()
@@ -68,20 +81,26 @@ class MpsParser:
             self.authorize(save_cookies=True)
 
     def top_similar_item(self) -> int:
+        """
+        Переход к ТОП-1 похожему товару
+        """
         self._driver.get(f"https://mpstats.io/wb/item/{self.wb_sku}")
         elements = self._driver.find_elements(
             By.XPATH, "//a[@href and @title='Открыть в Wildberries' and " "@target='_blank']"
         )
         return int([i.text for i in elements][0])
 
-    def get_keywords(self, sku: int):
+    def get_keywords(self, sku: int) -> Dict[str, int]:
+        """
+        Парсинг ключевых слов со страницы https://mpstats.io/wb/item/{sku}
+        """
         self._driver.get(f"https://mpstats.io/wb/item/{sku}")
         self._driver.execute_script("document.body.style.zoom = '30%'")
 
         kw_json = {}
         while len(kw_json) <= 30:
             scroll_table = WebDriverWait(self._driver, 10).until(
-                EC.visibility_of_element_located(
+                ec.visibility_of_element_located(
                     (
                         By.XPATH,
                         '//*[@id="mp-stats-app"]/div[2]/div/section/div[5]'
@@ -101,6 +120,8 @@ class MpsParser:
         json.dump(kw_json, open("keywords.json", "w", encoding="utf8"), ensure_ascii=False, indent=1)
         print("Json dumped")
 
+        return kw_json
+
     def service(self):
         try:
             self.auth_pipeline()
@@ -115,4 +136,4 @@ class MpsParser:
             self._driver.quit()
 
 
-MpsParser(wb_sku=78537829).service()
+MpsParser(wb_sku=99204322).service()
